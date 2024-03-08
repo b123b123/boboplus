31e11ff9eb909f03cb48053dedfb23bcefe3d5cf
b123b123 <02684200@163.com>
2024-03-08 10:01:01 +0800

    [new] 新增play

---
+++ play/vite.config.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: Play
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-08 09:58:55
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-08 09:59:23
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+	plugins: [vue()],
+});
