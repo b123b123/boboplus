98966b424b6f2c85dd1ca737f3401110c9fa96c9
b123b123 <02684200@163.com>
2024-03-08 10:05:40 +0800

    [update] play文件优化

---
+++ play/src/main.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: main.ts 入口文件
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-08 10:03:14
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-08 10:03:38
+ */
+import { createApp } from "vue";
+import App from "./app.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
