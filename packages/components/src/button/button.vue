d3add7b2a832ca641b37be5ff9c6265be2700a1f
b123b123 <02684200@163.com>
2024-03-11 16:04:44 +0800

    [opt] 测试

--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -4,10 +4,10 @@
  * @Author: WangBo
  * @Date: 2024-03-11 15:50:15
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-11 15:50:26
+ * @LastEditTime: 2024-03-11 16:01:34
 -->
 <template>
-	<div>button</div>
+	<button>测试按钮</button>
 </template>
 
 <script setup lang="ts"></script>
--- play/src/app.vue
+++ play/src/app.vue
@@ -1,3 +1,10 @@
 <template>
 	<div>启动测试</div>
+	<Button />
 </template>
+
+<script setup lang="ts">
+import { Button } from "@boboplus/components";
+</script>
+
+<style scoped></style>
