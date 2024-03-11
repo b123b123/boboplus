9d235f6b07aa881e14a22bdff2152993a1f2eecf
b123b123 <02684200@163.com>
2024-03-11 15:58:09 +0800

    [new] 新增组件库和工具库

---
+++ packages/components/src/button/button.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: button按钮
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-11 15:50:15
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-11 15:50:26
+-->
+<template>
+	<div>button</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ packages/components/src/input/input.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: input输入库
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-11 15:51:14
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-11 15:51:24
+-->
+<template>
+	<div>input</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
