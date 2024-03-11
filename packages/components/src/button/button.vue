f07f757523d5c7f559bef0d6f4e9509d57444ce1
b123b123 <02684200@163.com>
2024-03-11 17:44:51 +0800

    [update] button更新

--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -4,15 +4,25 @@
  * @Author: WangBo
  * @Date: 2024-03-11 15:50:15
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-11 16:49:44
+ * @LastEditTime: 2024-03-11 17:40:29
 -->
 <template>
-	<button>测试按钮</button>
+	<button class="ea-button" :class="buttonStyle"><slot /></button>
 </template>
 
-<script setup lang="ts">
-defineOptions({
-	name: "bo-button",
+<script lang="ts" setup>
+import "./index.less";
+import { computed } from "vue";
+defineOptions({ name: "bo-button" });
+
+type ButtonProps = {
+	type?: string;
+};
+
+const buttonProps = defineProps<ButtonProps>();
+
+const buttonStyle = computed(() => {
+	return { [`ea-button--${buttonProps.type}`]: buttonProps.type };
 });
 </script>
 
--- play/src/app.vue
+++ play/src/app.vue
@@ -4,12 +4,11 @@
  * @Author: WangBo
  * @Date: 2024-03-08 10:03:28
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-11 17:32:56
+ * @LastEditTime: 2024-03-11 17:41:53
 -->
 <template>
 	<div>启动测试</div>
-	<bo-button></bo-button>
-	<bo-input></bo-input>
+	<bo-button type="primary">启动测试</bo-button>
 </template>
 
 <script setup lang="ts"></script>
