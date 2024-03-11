d1fdbc8a5c1caac55a2c2fa451f111c7659cd100
b123b123 <02684200@163.com>
2024-03-11 17:31:35 +0800

    [opt]

--- play/src/app.vue
+++ play/src/app.vue
@@ -4,12 +4,10 @@
  * @Author: WangBo
  * @Date: 2024-03-08 10:03:28
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-11 16:51:24
+ * @LastEditTime: 2024-03-11 17:30:21
 -->
 <template>
 	<div>启动测试</div>
-	<bo-button></bo-button>
-	<bo-input></bo-input>
 </template>
 
 <script setup lang="ts"></script>
