42b6818c8c4f9c081a140c93da61db8782e425b0
b123b123 <02684200@163.com>
2024-03-15 16:25:23 +0800

    [new] gulp打包新增设置完成

--- packages/components/script/build/index.ts
+++ packages/components/script/build/index.ts
@@ -4,12 +4,16 @@
  * @Author: WangBo
  * @Date: 2024-03-15 15:58:09
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-15 16:00:29
+ * @LastEditTime: 2024-03-15 16:23:37
  */
+
 import delPath from "../utils/delpath";
-import { series } from "gulp";
-import { pkgPath } from "../utils/paths";
+import run from "../utils/run";
+import { series, parallel, src, dest } from "gulp";
+import { pkgPath, componentPath } from "../utils/paths";
 import { resolve } from "path";
+import less from "gulp-less";
+import autoprefixer from "gulp-autoprefixer";
 
 //删除 boboplus
 export const removeDist = () => {
@@ -17,4 +21,24 @@ export const removeDist = () => {
 	return delPath(folderPath);
 };
 
-export default series(async () => removeDist());
+// 打包less样式
+export const buildStyle = () => {
+	return src(`${componentPath}/src/**/**.less`)
+		.pipe(less())
+		.pipe(autoprefixer())
+		.pipe(dest(`${pkgPath}/boboplus/lib/src`))
+		.pipe(dest(`${pkgPath}/boboplus/es/src`));
+};
+
+//打包组件
+export const buildComponent = async () => {
+	run("pnpm run build", componentPath);
+};
+
+export default series(
+	async () => removeDist(),
+	parallel(
+		async () => buildStyle(),
+		async () => buildComponent()
+	)
+);
