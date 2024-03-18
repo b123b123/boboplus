a3de051e3fb8a547d2545409cd837e0e39ed5afc
b123b123 <02684200@163.com>
2024-03-18 10:26:39 +0800

    [new] 新增publish脚本完成

---
+++ packages/components/script/publish/index.ts
@@ -0,0 +1,22 @@
+/*
+ * @Description: Publish.ts
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-18 09:11:48
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-18 10:24:27
+ */
+import run from "../utils/run";
+import { pkgPath } from "../utils/paths";
+import { series, src, dest } from "gulp";
+
+export const copyPkg = async () => {
+	return src("./package.json").pipe(dest(`${pkgPath}/boboplus`));
+};
+
+export const publishComponent = async () => {
+	run("release-it", `${pkgPath}/boboplus`);
+};
+
+// export default series(copyPkg, async () => publishComponent());
+export default series(copyPkg);
