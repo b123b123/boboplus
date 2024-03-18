79ec65b733c614197f2b6acfde6e8433a9761601
b123b123 <02684200@163.com>
2024-03-18 10:40:50 +0800

    [update]

--- packages/boboplus/es/src/input/input.vue.mjs
+++
@@ -1,7 +0,0 @@
-import _sfc_main from "./input.vue2.mjs";
-import "./input.vue3.mjs";
-import _export_sfc from "../../_virtual/_plugin-vue_export-helper.mjs";
-const _Input = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-3f9eab94"]]);
-export {
-  _Input as default
-};
