5bc9cb00f784e6a749c08960cba004ecd1432c42
b123b123 <02684200@163.com>
2024-03-19 09:29:37 +0800

    [new] 新增vitepress

---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+	themeConfig: {
+		siteTitle: "boboplus",
+		nav: [
+			{ text: "指南", link: "/guild/installation" },
+			{ text: "组件", link: "/components/button/" },
+		],
+		socialLinks: [{ icon: "github", link: "https://github.com/b123b123/boboplus" }],
+	},
+};
---
+++ site/docs/index.md
@@ -0,0 +1,9 @@
+<!--
+ * @Description: Hello World
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-19 09:25:12
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-19 09:25:29
+-->
+# Hello boboplus
