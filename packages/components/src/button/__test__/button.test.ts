4fea7b65b14972fcafd48ff8f9f38a4b216d95cf
b123b123 <02684200@163.com>
2024-03-25 14:51:52 +0800

    [test]

---
+++ packages/components/src/button/__test__/button.test.ts
@@ -0,0 +1,37 @@
+/*
+ * @Description: button.ts - 测试
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2024-03-25 14:44:20
+ * @LastEditors: WangBo
+ * @LastEditTime: 2024-03-25 14:51:00
+ */
+import { describe, expect, it } from "vitest";
+
+describe("Helloboboplus", () => {
+    it("should be helloboboplus", () => {
+        expect("hello" + "boboplus").toBe("helloboboplus");
+    });
+
+    it("should be helloone", () => {
+        expect("hello" + "one").toBe("helloone");
+    });
+
+    it("should be hellotwo", () => {
+        expect("hello" + "two").toBe("hellotwo");
+    });
+});
+
+describe("Helloboboplus-one", () => {
+    it("should be helloboboplus", () => {
+        expect("hello" + "boboplus").toBe("helloboboplus");
+    });
+
+    it("should be helloone", () => {
+        expect("hello" + "one").toBe("helloone");
+    });
+
+    it("should be hellotwo", () => {
+        expect("hello" + "two").toBe("hellotwo");
+    });
+});
