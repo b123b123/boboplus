8081b589e9a1ed54a905fbfd9fbbdaa20a2bdd36
b123b123 <02684200@163.com>
2024-03-25 10:12:21 +0800

    [new] 新增eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:vue/vue3-essential", "plugin:@typescript-eslint/recommended"],
+	globals: {
+		defineOptions: true,
+	},
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		parser: "@typescript-eslint/parser",
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		"@typescript-eslint/ban-ts-comment": "off",
+		"vue/multi-word-component-names": "off",
+	},
+};
