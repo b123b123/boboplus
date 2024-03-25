375eaaaf23188ed94ca5026819a086102c1bf0c0
b123b123 <02684200@163.com>
2024-03-25 15:33:23 +0800

    [new] 新增vue组件测试

--- packages/components/src/button/__test__/button.test.ts
+++ packages/components/src/button/__test__/button.test.ts
@@ -4,34 +4,28 @@
  * @Author: WangBo
  * @Date: 2024-03-25 14:44:20
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-25 14:51:00
+ * @LastEditTime: 2024-03-25 15:32:53
  */
 import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import button from "../button.vue";
 
-describe("Helloboboplus", () => {
-    it("should be helloboboplus", () => {
-        expect("hello" + "boboplus").toBe("helloboboplus");
-    });
+// describe("Helloboboplus", () => {
+//     it("should be helloboboplus", () => {
+//         expect("hello" + "boboplus").toBe("helloboboplus");
+//     });
+// });
 
-    it("should be helloone", () => {
-        expect("hello" + "one").toBe("helloone");
-    });
+// The component to test
+describe("test button", () => {
+    it("should render slot", () => {
+        const wrapper = mount(button, {
+            slots: {
+                default: "boboplus"
+            }
+        });
 
-    it("should be hellotwo", () => {
-        expect("hello" + "two").toBe("hellotwo");
-    });
-});
-
-describe("Helloboboplus-one", () => {
-    it("should be helloboboplus", () => {
-        expect("hello" + "boboplus").toBe("helloboboplus");
-    });
-
-    it("should be helloone", () => {
-        expect("hello" + "one").toBe("helloone");
-    });
-
-    it("should be hellotwo", () => {
-        expect("hello" + "two").toBe("hellotwo");
+        // Assert the rendered text of the component
+        expect(wrapper.text()).toContain("boboplus");
     });
 });
