648880a476becaa0bf559f6edc8af29a38139b25
b123b123 <02684200@163.com>
2024-03-25 14:10:52 +0800

    [opt]

--- packages/components/src/input/input.vue
+++ packages/components/src/input/input.vue
@@ -4,10 +4,10 @@
  * @Author: WangBo
  * @Date: 2024-03-11 15:51:14
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-15 16:37:47
+ * @LastEditTime: 2024-03-25 14:10:45
 -->
 <template>
-    <input type="text" placeholder="请输入组件名称" class="inputClass" />
+    <input type="text" placeholder="请输入组件名称" class="input-class" />
 </template>
 
 <script setup lang="ts">
@@ -17,7 +17,7 @@ defineOptions({
 </script>
 
 <style scoped>
-.inputClass {
+.input-class {
     text-decoration: line-through;
     color: aquamarine;
 }
