c407c9c3bd05494a6504185447c89bffae1496bb
b123b123 <02684200@163.com>
2024-03-25 16:42:47 +0800

    [opt]

--- play/src/app.vue
+++ play/src/app.vue
@@ -4,11 +4,12 @@
  * @Author: WangBo
  * @Date: 2024-03-08 10:03:28
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-11 17:41:53
+ * @LastEditTime: 2024-03-25 16:40:01
 -->
 <template>
     <div>启动测试</div>
     <bo-button type="primary">启动测试</bo-button>
+    <bo-input></bo-input>
 </template>
 
 <script setup lang="ts"></script>
--- play/vite.config.ts
+++ play/vite.config.ts
@@ -4,12 +4,13 @@
  * @Author: WangBo
  * @Date: 2024-03-08 09:58:55
  * @LastEditors: WangBo
- * @LastEditTime: 2024-03-12 14:24:15
+ * @LastEditTime: 2024-03-25 16:42:11
  */
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import DefineOptions from "unplugin-vue-define-options/vite";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
-    plugins: [vue(), DefineOptions()]
+    plugins: [vue(), vueJsx(), DefineOptions()]
 });
