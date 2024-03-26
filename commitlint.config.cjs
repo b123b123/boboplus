ea5c22352a2ba1f93bff8d4279f9063297960b42
b123b123 <02684200@163.com>
2024-03-26 13:50:57 +0800

    [update] 提交配置更新优化完成

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
                 "build", // 打包
                 "ci", // 持续集成
                 "wip", // 工作进行中的提交
-                "[new]", // 新功能
+                "new", // 新功能
                 "style",
                 "fix",
                 "opt",
@@ -40,7 +40,7 @@ module.exports = {
     },
     parserPreset: {
         parserOpts: {
-            headerPattern: /^\[?(\w+)\]?\s(.*)$/, // 设置提交消息的正则匹配规则，允许方括号包裹类型
+            headerPattern: /^\[(\w+)\]\s(.*)$/, // 设置提交消息的正则匹配规则，允许方括号包裹类型
             headerCorrespondence: ["type", "subject"]
         }
     }
